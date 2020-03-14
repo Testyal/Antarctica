b59423650b98a2c17a632af22bf5902a046b301e
Billy <williamvrossii@gmail.com>
2020-03-14 09:19:28 +0000

    Add CameraMovement script.

--- Penguin Game/Assets/CameraMovement.cs
+++ Penguin Game/Assets/CameraMovement.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    [SerializeField] private GameObject follow;
 
+    private void Update()
+    {
+        this.transform.position = this.follow.transform.position + Vector3.back;
+    }
 }
